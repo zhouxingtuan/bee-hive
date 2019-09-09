49f64706915914f0911e19d384e9d60ff4005042
zxt <zxt@juhu.com>
2019-09-09 20:20:30 +0800

    c

--- proxy/proxy8001.js
+++ proxy/proxy8001.js
@@ -13,46 +13,93 @@ const ws = require('nodejs-websocket')
 let server = ws.createServer(connection => {
 // 如果到access的连接还没建立，那么从ws收到的数据需要先缓存下来
 var tempCache = new Buffer(0);
-connection.on('binary', function(inStream) {
-  // 创建空的buffer对象，收集二进制数据
-  var data = new Buffer(0)
-  // 读取二进制数据的内容并且添加到buffer中
-  inStream.on('readable', function() {
-    var newData = inStream.read()
-    if (newData){
-      data = Buffer.concat([data, newData], data.length + newData.length)
+var acceptConn;
+var acceptState = 0;
+function _closeConnection(){
+    if(connection != null){
+        var c = connection;
+        connection = null;
+        c.close();
     }
-  })
-  inStream.on('end', function() {
-    // 读取完成二进制数据后，处理二进制数据
-//        process_my_data(data)
-    console.log("receive data " + data.byteLength);
-  })
-})
-connection.on('connection', function(conn) {
-  console.log('connection open OK', conn)
-  // 编写socket连接到access服务
+}
+function _closeAcceptConn(){
+    if(acceptConn != null){
+        var ac = acceptConn;
+        acceptConn = null;
+        ac.close();
+    }
+}
+function _openAcceptConn(){
+    if(acceptConn != null){
+        console.log("connect is already open");
+        return;
+    }
+    acceptConn = net.connect({port: access_port, host: access_host, }, function() {
+        acceptState = 2;
+        console.log('connect OK to Access');
+        // tempCache的数据发送到access
 
+    });
+    acceptConn.on('data', function(data) {
+        console.log("recv data", data.byteLength);
+        // 发送消息到 connection 客户端
 
-})
-connection.on('connect', function(code) {
-  console.log('connect open', code)
-  // 编写socket连接到access服务
+    });
+    acceptConn.on('end', function() {
+        acceptState = 3;
+        console.log('close connect to access');
+        acceptConn = null;
+        _closeConnection();
+    });
+    acceptState = 1;
+}
+function _sendDataToAccess(data){
+    if(acceptState == 0){
+        // 第一个数据包丢弃 8 字节，剩余的数据进入tempCache
 
+    }else if(acceptState == 1){
+        // 数据进入tempCache
 
+    }else if(acceptState == 2){
+        // 直接转发数据
+
+    }else{
+        // 3 不做处理
+    }
+}
+connection.on('binary', function(inStream) {
+    // 创建空的buffer对象，收集二进制数据
+    var data = new Buffer(0);
+    // 读取二进制数据的内容并且添加到buffer中
+    inStream.on('readable', function() {
+        var newData = inStream.read()
+        if (newData){
+            data = Buffer.concat([data, newData], data.length + newData.length)
+        }
+    })
+    inStream.on('end', function() {
+        // 读取完成二进制数据后，处理二进制数据
+        //        process_my_data(data)
+        console.log("receive data " + data.byteLength);
+        _sendDataToAccess(data);
+        if(acceptConn == null){
+            _openAcceptConn();
+        }
+    })
 })
 connection.on('close', function(code) {
-  console.log('connect close', code)
-  // 这里需要关闭到access的socket连接
-
+    console.log('connect close', code)
+    // 这里需要关闭到access的socket连接
+    connection = null;
+    _closeAcceptConn();
 })
 connection.on('error', function(code) {
-  console.log('connect error', code)
-  // 某些情况如果客户端多次触发连接关闭，会导致connection.close()出现异常，这里try/catch一下
-  try {
-    connection.close()
-  } catch (error) {
-    console.log('close exception', error)
-  }
+    console.log('connect error', code)
+    // 某些情况如果客户端多次触发连接关闭，会导致connection.close()出现异常，这里try/catch一下
+    try {
+        _closeConnection();
+    } catch (error) {
+        console.log('close exception', error)
+    }
 })
 }).listen(listen_port, listen_host)
